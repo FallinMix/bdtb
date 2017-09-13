13eaf89b7804942e647c57a04d98193b48ace27c
FallinMix <517837002@qq.com>
2017-09-13 20:09:06 +0800

    add get content

--- bdtb.py
+++ bdtb.py
@@ -16,13 +16,40 @@ class BDTB():
 	 		url = self.baseURL + self.seeLz + '&pn=' + str(pageNum)
 	 		request = urllib2.Request(url)
 	 		response = urllib2.urlopen(request)
-	 		print response.read()
-	 		return response
+	 		#print response.read()
+	 		return response.read()
 	 	except urllib2.URLError as e:
 	 		if hasattr(e, "reason"):
 	 			print u"连接百度贴吧失败，错误原因：", e.reason
 	 			return None
 
+	def getResultGroup(self, result):
+	 	if result:
+	 		print result.group(1)
+	 		return result.group(1).strip()
+	 	else:
+	 		return None
+
+	def getTitle(self):
+	 	page = self.getPage(1)
+	 	pattern = re.compile('<h1 class="core_title_txt.*?>(.*?)</h1>', re.S)
+	 	result = re.search(pattern, page)
+	 	return self.getResultGroup(result)
+
+	def getPageNum(self):
+	 	page = self.getPage(1)
+	 	pattern = re.compile('<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>',re.S)
+	 	result = re.search(pattern, page)
+	 	return self.getResultGroup(result)
+
+	def getContent(self, page):
+		pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
+		items = re.findall(pattern, page)
+		for item in items:
+			print item
+
+
 bdtb = BDTB(baseURL, 1)
-bdtb.getPage(1)
+page = bdtb.getPage(1)
+bdtb.getContent(page)
 		
